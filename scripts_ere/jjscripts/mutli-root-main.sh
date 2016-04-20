5ee91f2f53bf136769b462365d83fd07068b99ef
vnogin <vnogin@mirantis.com>
2016-04-20 14:42:00 +0300

    Main script

--- scripts_ere/jjscripts/mutli-root-main.sh
+++ scripts_ere/jjscripts/mutli-root-main.sh
@@ -1,58 +1,134 @@
 #!/bin/bash
+#===============================================================================
+#
+#          FILE:  multi-root-main.sh
+#
+#         USAGE:  ./multi-root-main.sh --conf <path to configuration file>
+#
+#   DESCRIPTION:
+#
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Vitalii Nogin	vnogin@mirantis.com
+#       COMPANY:  Mirantis Inc.
+#       VERSION:  1.0
+#       CREATED:  20/04/2016 14:31:01 PM MDT
+#      REVISION:  ---
+#===============================================================================
+
 usage ()
 {
   echo 'Usage : ./multi-root-main.sh' 
-  echo '		--user 		<user name> '
-  echo '		--job-mode 	<job mode (create backup/restore from the backup/cetera)> '
-  echo '                --conf-file 	<path to the local.conf file> '
-  echo '		--git 		<link to relevant github, for instance https://github.com/ingwarr/shellhorse.git>'
+  echo '		--conf 		<path to configuration file> '
   exit
 }
 
+config_error()
+{
+  echo "ERROR. Please check your configuration file as it seems something hasn't been defined."
+  exit
+}
+
+
 let AMOUNT_INPUT_PARAMS=$#
 
-#if [ "$AMOUNT_INPUT_PARAMS" -ge 4 ]
-#then
-#  usage
-#fi
+if [ "${AMOUNT_INPUT_PARAMS}" -lt 2 ]
+then
+  usage
+fi
+
+if [ "${AMOUNT_INPUT_PARAMS}" -gt 3 ]
+then
+  usage
+fi
 
 while [ "$1" != "" ]; do
-case $1 in
-        --user )       shift
-                       USER_ID=$1
-                       ;;
-        --job-mode )   shift
-                       JOB_MODE=$1
-                       ;;
-        --conf-file )  shift
-                       CONF_FILE=$1
-                       ;;
-        --git )        shift
-                       GIT_LINK=$1
+    case $1 in
+        --conf-file )       shift
+                       CONFIG_FILE=$1
                        ;;
     esac
     shift
 done
 
-# extra validation 
-#if [ "$USER_ID" = "" ]
-#then
-#    usage
-#fi
+# Empty parameter validation 
+if [ "${CONFIG_FILE}" = "" ]
+then
+    usage
+fi
 
-#if [ "$JOB_MODE" = "" ]
-#then
-#    usage
-#fi
+# File exists check
+if [ -s "${CONFIG_FILE}" ]
+then
+	CONFIG_OPTIONS=`cat ${CONFIG_FILE}`
+else
+	echo "Please provide correct path to the configuration file as ${CONFIG_FILE} file doesn't exist or empty"
+fi
 
-#if [ "$CONF_FILE" = "" ]
-#then
-#    usage
-#fi
+source ${CONFIG_FILE}
 
-#if [ "$GIT_LINK" = "" ]
-#then
-#    usage
-#fi
+#Mandatory parameters are defined in configuration file
 
-echo "USER = $USER_ID, JOB_MODE = $JOB_MODE, CONF_FILE = $CONF_FILE, GIT_LINK = $GIT_LINK"
+if [ "${USER_ID}" = "" ]
+then
+    config_error
+fi
+
+if [ "${JOB_MODE}" = "" ]
+then
+    config_error
+fi
+
+case ${JOB_MODE} in
+  upgrade)
+		echo "Running upgrade mode"
+		;;
+  backup)
+                echo "Running backup mode"
+                ;;
+  list)
+                echo "Running list mode"
+                ;;
+  restore)
+                echo "Running restore mode"
+                ;;
+  standart)
+                echo "Running standart mode"
+                ;;
+  deploy)
+                echo "Running deploy mode"
+                ;;
+  *)  
+      echo "ERROR: ${JOB_MODE} mode isn't supported. Please check your configuration file." 
+      exit
+      ;; 
+esac
+
+
+echo "USER-ID from config is ${USER_ID} JOB_MODE=${JOB_MODE} GIT_LINK=${GIT_LINK} IF_DEPLOYMENT_FAIL_JOB=${IF_DEPLOYMENT_FAIL_JOB}"
+echo "CONFIG FILE = ${CONFIG_FILE}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
