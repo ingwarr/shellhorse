dad861608ac7fce9dcd08cd4505ecd2dae3ddc96
Igor Pukha <sorceress1979@gmail.com>
2016-12-02 17:26:13 +0200

    I've added clearing for begin and end of the physical disk

--- scripts/part_clean.sh
+++ scripts/part_clean.sh
@@ -3,8 +3,11 @@
 block_count=2048
 block_size=512
 
+
 for drive in `lshw -class disk|awk '/logical / {print $3}'`
     do
+        disk_size=`sfdisk -l /dev/sda|awk '/Disk / {print $5 }'`
+        let "disk_size_blocks = disk_size / 512"
         part_per_disk=`sfdisk -l|grep "^${drive}"|wc -l`
         echo ${part_per_disk}
         echo "$i"
@@ -23,8 +26,12 @@ for drive in `lshw -class disk|awk '/logical / {print $3}'`
 			        let "seek_offset = field - block_count"
                                 f_num=0
                         fi
-                        echo "dd if=/dev/zero of=${drive} bs=${block_size} seek=${seek_offset} count=${block_count}"
+                        dd if=/dev/zero of=${drive} bs=${block_size} seek=${seek_offset} count=${block_count}
                     done
 	    done
+        let "seek_offset = disk_size_blocks - block_count" 
+        dd if=/dev/zero of=${drive} bs=${block_size} seek=${seek_offset} count=${block_count}
+        seek_offset=0
+        dd if=/dev/zero of=${drive} bs=${block_size} seek=${seek_offset} count=${block_count}
     done
 
